97eebce17f415129cd378c4623c77e112012f479
raven4752 <raven4752@foxmail.com>
2019-04-08 11:06:37 +0800

    Update input_online.py
    update word vector reading

--- input_online.py
+++ input_online.py
@@ -18,4 +18,6 @@ if not os.path.exists(model_dir):
 
 # 开始定义df1 ~ df4
 df1 = pd.read_csv("input/data_tr.csv", encoding='utf-8')
-df2 = pd.read_csv('input/w2v.csv', encoding='utf-8', header=None)
+#df2 = pd.read_csv('input/w2v.csv', encoding='utf-8', header=None) 
+# 标准词向量格式文件读取方法，注意不能有空白字符的词向量。
+df2 = pd.read_csv('input/w2v.csv', encoding='utf-8', header=None, sep=' ', quoting=3, skiprows=1) 
